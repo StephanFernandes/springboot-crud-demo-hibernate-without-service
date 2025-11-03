afa350a25ede5474c1a14a0be60082a805128eba
Stephan Bastyav Fernandes <stephanfernandes4@gmail.com>
2025-11-03 19:37:02 +0530

    Initial commit

---
+++ src/main/java/com/user/manage/controller/UserController.java
@@ -0,0 +1,75 @@
+package com.user.manage.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.user.manage.entity.User;
+import com.user.manage.exception.ResourceNotFoundException;
+import com.user.manage.repository.UserRepository;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+	
+
+	@Autowired
+	private UserRepository userRepository;
+	//get all user
+	
+	@GetMapping
+	public List<User> getAllUsers(){
+		return this.userRepository.findAll();
+	}
+	
+	
+	//get user by id
+	@GetMapping("/{id}")
+	public User getUserById(@PathVariable (value = "id") long userId) {
+		return this.userRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User not found with id: "+userId));
+				
+	}
+	//.orElseThrow(() -> ResourceNotFoundException("User Not Found with id:"+ userId));
+	//create user
+	@PostMapping
+	public User CreateUser(@RequestBody User user) {
+		return this.userRepository.save(user);
+	}
+	// update user 
+	@PutMapping("/{id}")
+	public User UpdateUser(@RequestBody User user, @PathVariable (value = "id") long userId) {
+		User existingUser = this.userRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User not found with id: "+userId));
+		existingUser.setFisrtName(user.getFisrtName());
+		existingUser.setLastName(user.getLastName());
+		existingUser.setEmail(user.getEmail());
+		return this.userRepository.save(existingUser);
+		 
+	}
+	//dlete user by id
+	@DeleteMapping("/{id}")
+	public ResponseEntity<User> DeleteUser(@PathVariable (value = "id") long userId) {
+		User deleteUser = this.userRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User not found with id: "+userId));
+		
+		this.userRepository.delete(deleteUser);
+		return ResponseEntity.ok().build();
+		
+		
+		
+		
+		
+		
+	}
+	
+}
---
+++ src/main/java/com/user/manage/entity/User.java
@@ -0,0 +1,72 @@
+package com.user.manage.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users") 
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	@Column(name="first_name")
+	private String firstName;
+	@Column(name="last_name")
+	private String lastName;
+	@Column(name="email")
+	private String email;
+	
+	//id i removed bca we will be creating id incremental order
+	public User(String fisrtName, String lastName, String email) {
+		super();
+		this.firstName = fisrtName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFisrtName() {
+		return firstName;
+	}
+
+	public void setFisrtName(String fisrtName) {
+		this.firstName = fisrtName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", fisrtName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+	
+	
+	
+
+}
